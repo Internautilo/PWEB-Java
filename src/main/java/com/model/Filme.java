37baf6757876c51d75110c2a2e1308c803522f6b
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-06-19 12:51:17 -0300

    fix: reflected changes from the database to the model Filme

--- src/main/java/com/model/Filme.java
+++ src/main/java/com/model/Filme.java
@@ -3,15 +3,36 @@ package com.model;
 public class Filme {
     public int id;
     public String titulo;
+    public String descricao;
     public String diretor;
     public String genero;
-    public float nota;
+    public String imagem;
+    public double nota;
 
-    public Filme(int id, String titulo, String diretor, String genero, float nota) {
+    public Filme(int id, String titulo, String descricao, String diretor, String genero, String imagem, double nota) {
         this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.diretor = diretor;
+        this.genero = genero;
+        this.imagem = imagem;
+        this.nota = nota;
+    }
+
+    public Filme(String titulo, String descricao, String diretor, String genero, String imagem) {
+        this.id = id;
+        this.titulo = titulo;
+        this.descricao = descricao;
+        this.diretor = diretor;
+        this.genero = genero;
+        this.imagem = imagem;
+    }
+
+    public Filme(String titulo, String diretor, String genero, String imagem, double nota) {
         this.titulo = titulo;
         this.diretor = diretor;
         this.genero = genero;
+        this.imagem = imagem;
         this.nota = nota;
     }
 
