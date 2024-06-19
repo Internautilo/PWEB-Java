f067f8cb50f6745d912eb47e8e016c7c50371e76
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-06-19 12:23:37 -0300

    refactor: changed ctor param and added new ctor

--- src/main/java/com/model/Usuario.java
+++ src/main/java/com/model/Usuario.java
@@ -7,15 +7,19 @@ public class Usuario {
     public String nome;
     protected boolean isAdmin = false;
 
-    public Usuario(int id, String email, String senha, String nome) {
+    public Usuario(int id, String nome, String email, String senha) {
         this.id = id;
         this.email = email;
         this.senha = senha;
         this.nome = nome;
     }
-    public Usuario(String email, String senha, String nome) {
+    public Usuario(String nome, String email, String senha) {
+        this.nome = nome;
         this.email = email;
         this.senha = senha;
-        this.nome = nome;
+    }
+    public Usuario(String email, String senha) {
+        this.email = email;
+        this.senha = senha;
     }
 }
