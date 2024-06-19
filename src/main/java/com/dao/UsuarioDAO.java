0e26a351d1804c9d4bdd2a7119dcfaf67ba089ad
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-06-19 12:28:36 -0300

    refactor: changed functions to static

--- src/main/java/com/dao/UsuarioDAO.java
+++ src/main/java/com/dao/UsuarioDAO.java
@@ -2,61 +2,81 @@ package com.dao;
 
 import com.database.DBQuery;
 import com.model.Usuario;
+import com.service.Hash;
 
+import javax.xml.transform.Result;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
-public class UsuarioDAO implements DAOInterface {
+public class UsuarioDAO {
     private DBQuery dbQuery;
 
     public UsuarioDAO() {
         this.dbQuery = new DBQuery();
     }
 
-
-    @Override
-    public int inserir(Object object) throws Exception {
-        if (!(object instanceof Usuario)) {
-            throw new Exception("Objeto nao e uma instancia de Usuario: ");
+    public static int create_user(Usuario usuario) {
+        String senhaHash = Hash.hash_string(usuario.senha);
+        if (senhaHash == null) {
+            throw new RuntimeException("Erro ao hashear senha");
         }
-        Usuario usuario = (Usuario) object;
-        String[] valores = {usuario.nome, usuario.email, usuario.senha};
-        return this.dbQuery.insert_query("users", "nome, email, senha", valores);
+        String[] valores = {usuario.nome, usuario.email, senhaHash};
+        return DBQuery.insert_query("users", "nome, email, senha", valores);
     }
 
-    @Override
-    public ResultSet listar_por_id(int[] ids) {
-        StringBuilder idList = new StringBuilder();
-        for (int i = 0; i < ids.length; i++) {
-            idList.append(ids[i]);
-            if (i < ids.length - 1) {
-                idList.append(",");
-            }
+    public static Usuario get_user_by_id(int id) throws SQLException {
+        ResultSet rs = DBQuery.select_query("users", "WHERE idUsuario = " + id + " AND removido != 1 LIMIT 1");
+        Usuario usuario = null;
+        if (rs.next()) {
+            usuario = new Usuario(
+                    rs.getInt("idUsuario"),
+                    rs.getString("nome"),
+                    rs.getString("email"),
+                    rs.getString("senha")
+            );
         }
-        String extraQuery = "WHERE idUsuario IN (" + idList.toString() + ") AND removido != 1";
-        return this.dbQuery.select_query("users", extraQuery);
+        return usuario;
     }
 
-    @Override
-    public ResultSet listar() {
-        return this.dbQuery.select_query("users", "WHERE removido != 1 ORDER BY idUsuario");
+    public static Usuario get_user_by_email(String email) throws SQLException {
+        ResultSet rs = DBQuery.select_query("users", "WHERE email = '" + email + "' AND removido != 1 LIMIT 1");
+        Usuario usuario = null;
+        if (rs.next()) {
+            usuario = new Usuario(
+                    rs.getInt("idUsuario"),
+                    rs.getString("nome"),
+                    rs.getString("email"),
+                    rs.getString("senha")
+            );
+        }
+        return usuario;
     }
 
-    @Override
-    public int alterar(int id, Object object) throws Exception {
-        if (!(object instanceof Usuario)) {
-            throw new Exception("Objeto nao e uma instancia de usuario");
+    public static List<Usuario> list_users() throws SQLException {
+        ResultSet rs =  DBQuery.select_query("users", "WHERE removido != 1 ORDER BY idUsuario");
+        List<Usuario> usuarios = new ArrayList<>();
+        while (rs.next()) {
+            usuarios.add(new Usuario(rs.getInt("idUsuario"),
+                    rs.getString("nome"),
+                    rs.getString("email"),
+                    rs.getString("senha"))
+            );
         }
-        Usuario usuario = (Usuario) object;
+        return usuarios;
+    }
+
+    public static int update_user(int id, Usuario usuario) throws Exception {
         String campos = "nome = '" + usuario.nome+ "', email = '" + usuario.email + "', senha = '" + usuario.senha + "'";
         String where = "WHERE idUsuario = " + id;
-        return this.dbQuery.update_query("users", campos, where);
+        return DBQuery.update_query("users", campos, where);
     }
 
-    @Override
-    public int remover(int id) {
-        return this.dbQuery.delete_query("users", id);
+    public static int delete_user(int id) {
+        return DBQuery.delete_query("users", id);
     }
-
 }
