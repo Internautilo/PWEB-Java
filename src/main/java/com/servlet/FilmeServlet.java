9993440b03771ad038a1a762a9e750770332226c
Yuri N Bertulucci <yuribertulucci2004@gmail.com>
2024-06-19 15:53:05 -0300

    feat: add img insertion to FilmeServlet

--- src/main/java/com/servlet/FilmeServlet.java
+++ src/main/java/com/servlet/FilmeServlet.java
@@ -3,15 +3,19 @@ package com.servlet;
 import com.dao.FilmeDAO;
 import com.model.Filme;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
 
-import java.io.IOException;
+import java.io.*;
 
 @WebServlet(urlPatterns = {"/filme/insert", "/filme/update", "/filme/delete"})
+@MultipartConfig(fileSizeThreshold = 1024*1024*50, maxFileSize = 1024*1024*50, maxRequestSize = 1024*1024*100)
 public class FilmeServlet extends HttpServlet {
+    private static final String UPLOAD_DIR = "WEB-INF" + File.separator + "upload";
 
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -19,6 +23,7 @@ public class FilmeServlet extends HttpServlet {
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String path = request.getServletPath();
+        PrintWriter writer = response.getWriter();
         switch (path) {
             case "/filme/insert":
                 insert_filme(request, response);
@@ -32,14 +37,25 @@ public class FilmeServlet extends HttpServlet {
         }
     }
 
-    private void insert_filme(HttpServletRequest request, HttpServletResponse response) {
+    private void insert_filme(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String titulo = request.getParameter("titulo");
         String descricao = request.getParameter("descricao");
         String diretor = request.getParameter("diretor");
         String genero = request.getParameter("genero");
-        String imagem = request.getParameter("imagem");
-        Filme filme = new Filme(titulo, descricao, diretor, genero, imagem);
 
+        // INSERÇÃO DE IMAGEM
+        Part filePart = request.getPart("imagem");
+        String fileName = filePart.getSubmittedFileName();
+        String currentPath = request.getServletContext().getRealPath(UPLOAD_DIR);
+        File uploadDir = new File(currentPath);
+        if (!uploadDir.exists()) {
+            uploadDir.mkdirs();
+        }
+        for (Part part : request.getParts()) {
+            part.write(currentPath + File.separator + fileName);
+        }
+
+        Filme filme = new Filme(titulo, descricao, diretor, genero, fileName);
         FilmeDAO.insert_filme(filme);
     }
 
